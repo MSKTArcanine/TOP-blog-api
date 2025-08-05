6122cdfc5a66e8b13fcd53e80ef5503ae945cb5c
MSKTArcanine <marcantoine1999@live.fr>
2025-08-05 19:57:26 +0200

    added editRouter methods

--- Routes/api/v1/editRouter.js
+++ Routes/api/v1/editRouter.js
@@ -1,17 +1,2 @@
-const prisma = require('../../../db/auth/connection');
+const authorController = require('../../../Controllers/api/v1/blogEditController');
 
-const getAllPostsByAuthorId = async (id) => await prisma.posts.findMany({
-    where:{
-        author_id: id,
-    }
-});
-
-const getPostByIdPROTECTED = async (id) => await prisma.posts.findUnique({
-    where:{id}
-});
-
-const getCommentsByPostId = async (id) => await prisma.comments.findMany({
-    where:{post_id: id}
-});
-
-module.exports = {getAllPostsByAuthorId, getCommentsByPostId, getPostByIdPROTECTED};
