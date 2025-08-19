4859d638d93d42a19225a1d918472dec92a039ac
Marc-AntoineSP <marcantoine.saintpierre@viacesi.fr>
2025-08-19 12:56:29 +0200

    passed username to blogAll route

--- Controllers/api/v1/blogAllController.js
+++ Controllers/api/v1/blogAllController.js
@@ -3,19 +3,19 @@ const comments = require('../../../db/api/v1/blog-all/comments');
 
 const getAllPosts = async (req, res) => {
     const result = await posts.getAllPosts();
-    res.json({posts: result});
+    res.json({posts: result, username: req.user.username});
 }
 
 const getPostById = async (req, res) => {
     const id = Number(req.params.id);
     const result = await posts.getPostById(id);
-    res.json({post: result});
+    res.json({post: result, username: req.user.username});
 }
 
 const getAllCommentsByPostId = async (req, res) => {
     const id = Number(req.params.id);
     const comms = await comments.getAllCommentByPostId(id);
-    res.json({comments: comms});
+    res.json({comments: comms, username: req.user.username});
 }
 
 module.exports = {getAllPosts, getPostById, getAllCommentsByPostId}
